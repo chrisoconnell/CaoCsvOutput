8cbfc10f0f76be50de2d0cfc36ef31bc63d0d430
Chris O'Connell <chris@chrisoconnell.info>
2012-12-10 22:04:54 -0800

    Create Module.php

---
+++ Module.php
@@ -0,0 +1,29 @@
+<?php
+/**
+* Zend Framework 2 View Helper to render an HTML Table
+*
+* @link https://github.com/chrisoconnell/CaoCsvOutput.git
+*/
+namespace CaoCsvOutput;
+
+class Module
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+}
